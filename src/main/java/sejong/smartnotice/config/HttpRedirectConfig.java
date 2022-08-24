fd80fb712f3864a7eb7e43e87876729e988985cc
eheh12321 <eheh12321@gmail.com>
2022-08-24 14:05:11 +0900

    application.properties 파일 분리

--- src/main/java/sejong/smartnotice/config/HttpRedirectConfig.java
+++ src/main/java/sejong/smartnotice/config/HttpRedirectConfig.java
@@ -9,32 +9,32 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class HttpRedirectConfig {
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        factory.addAdditionalTomcatConnectors(createConnector());
-        return factory;
-    }
-
-    private Connector createConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setSecure(false);
-        connector.setPort(8080);
-        connector.setRedirectPort(443);
-        return connector;
-    }
-}
+//@Configuration
+//public class HttpRedirectConfig {
+//
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//        factory.addAdditionalTomcatConnectors(createConnector());
+//        return factory;
+//    }
+//
+//    private Connector createConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setSecure(false);
+//        connector.setPort(8080);
+//        connector.setRedirectPort(443);
+//        return connector;
+//    }
+//}
