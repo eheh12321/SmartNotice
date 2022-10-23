a405317ca3008b891c06eee701d09680fa9d1787
eheh12321 <eheh12321@gmail.com>
2022-10-24 01:24:26 +0900

    파일 저장 대체 왜 안되는지 에러 로그 출력

--- src/main/java/sejong/smartnotice/service/AnnounceService.java
+++ src/main/java/sejong/smartnotice/service/AnnounceService.java
@@ -202,6 +202,7 @@ public class AnnounceService {
             log.info("파일 저장에 성공했습니다");
             return true;
         } catch (Exception e) {
+            e.printStackTrace();
             log.warn("파일 저장에 실패하였습니다");
             return false;
         }
