d74353aa63468b0c0a2f1786ca0919794cf4307b
eheh12321 <eheh12321@gmail.com>
2022-10-23 20:24:27 +0900

    HTTPS 인증서 재발급 및 스크립트 적용

--- scripts/start.sh
+++ scripts/start.sh
@@ -28,6 +28,6 @@ IDLE_PROFILE=$(find_idle_profile)
 echo "> $JAR_NAME 을 profile=$IDLE_PROFILE 로 실행합니다"
 
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-api.properties \
+        -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-api.properties,/home/ec2-user/app/application-https.properties \
         -Dspring.profiles.active=$IDLE_PROFILE \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
