488d5fab1083d4f0966322f3674bd432a95652f4
eheh12321 <eheh12321@gmail.com>
2022-10-24 00:18:17 +0900

    배포 스크립트에 sudo 권한 추가
    - Caused by: java.net.SocketException: 허가 거부 이슈
    
    * https://galid1.tistory.com/632

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,7 +22,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar \
+nohup sudo java -jar \
         -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties,/home/ec2-user/app/application-api.properties,/home/ec2-user/app/application-https.properties \
         -Dspring.profiles.active=real \
         $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
