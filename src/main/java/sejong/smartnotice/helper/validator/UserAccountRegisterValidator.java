ab722750a3c15dcc123ffb1539cdfdc56a5fb16e
eheh12321 <eheh12321@gmail.com>
2022-11-17 01:28:13 +0900

    refactor: 회원 가입 비즈니스 로직 Validator 추가 (#3)
    - 전화번호, 아이디 중복 오류 검증
      - 아이디는 관리자, 주민, 보호자 통틀어서 전부 유니크 해야하기 때문에 UserAccountRepository에서 조회 및 처리해야 함
    - 보호자 추가 로직을 저렇게 instanceof로 비교하고 한꺼번에 같이 넣어놓는게 좋은 방법인지는 모르겠으나 한군데서 관련 검증 로직을 전부 볼 수 있다는 장점 하나는 있는듯

---
+++ src/main/java/sejong/smartnotice/helper/validator/UserAccountRegisterValidator.java
@@ -0,0 +1,44 @@
+package sejong.smartnotice.helper.validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import sejong.smartnotice.domain.member.User;
+import sejong.smartnotice.dto.request.SupporterRegisterDTO;
+import sejong.smartnotice.dto.request.UserAccountRegisterDTO;
+import sejong.smartnotice.repository.UserAccountRepository;
+
+@RequiredArgsConstructor
+@Component
+public class UserAccountRegisterValidator implements Validator {
+
+    private final UserAccountRepository userAccountRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return UserAccountRegisterDTO.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        UserAccountRegisterDTO dto = (UserAccountRegisterDTO) target;
+        if (userAccountRepository.findByAccount_LoginId(dto.getLoginId()).isPresent()) {
+            errors.rejectValue("loginId", "아이디 중복 오류", "이미 사용중인 아이디입니다");
+        }
+        if (userAccountRepository.findByTel(dto.getTel()).isPresent()) {
+            errors.rejectValue("tel", "전화번호 중복 오류", "이미 사용중인 전화번호입니다");
+        }
+
+        // 보호자 추가 로직
+        if(dto instanceof SupporterRegisterDTO) {
+            SupporterRegisterDTO supporterRegisterDTO = (SupporterRegisterDTO) dto;
+            if(userAccountRepository.findByTel(supporterRegisterDTO.getUserTel())
+                    .filter(userAccount -> userAccount instanceof User)
+                    .isEmpty()) {
+                errors.rejectValue("userTel", "마을주민 없음 오류", "마을 주민이 존재하지 않습니다");
+            }
+        }
+    }
+}
+
