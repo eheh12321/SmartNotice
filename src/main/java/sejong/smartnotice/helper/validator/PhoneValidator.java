1f4325d651db4b69312b5c038eeea43bc83daf35
eheh12321 <eheh12321@gmail.com>
2022-11-17 01:23:40 +0900

    refactor: 핸드폰 번호 검증 애노테이션(Phone) 추가 (#3)
    - 000-000(0)-0000 형태 고정

---
+++ src/main/java/sejong/smartnotice/helper/validator/PhoneValidator.java
@@ -0,0 +1,17 @@
+package sejong.smartnotice.helper.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PhoneValidator implements ConstraintValidator<Phone, String> {
+
+    @Override
+    public void initialize(Phone constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return value.matches("\\d{3}-\\d{3,4}-\\d{4}");
+    }
+}
