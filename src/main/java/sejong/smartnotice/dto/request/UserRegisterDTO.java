e5a305e0b17c82e60eece21ac025379415ddb1cc
eheh12321 <eheh12321@gmail.com>
2022-11-17 01:22:29 +0900

    refactor: 회원가입시 사용하는 DTO 정리 (#3)
    - UserRegisterDTO가 추상 클래스로서 UserAccount가 가진 공통 컬럼들을 모두 처리함
    - 이를 관리자, 주민, 보호자 RegisterDTO가 각각 상속받아서 사용하게 됨
      - 굳이 이렇게 해야하나 싶긴 했는데, Validator를 하나로 통합해서 사용하기 위해서 이렇게 만들어봄.
      - 코드 중복 좀 줄여보겠다고 했다가 더 복잡한 코드가 나온 것 같은 느낌...

---
+++ src/main/java/sejong/smartnotice/dto/request/UserRegisterDTO.java
@@ -0,0 +1,29 @@
+package sejong.smartnotice.dto.request;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter @Setter
+public class UserRegisterDTO extends UserAccountRegisterDTO {
+
+    @NotBlank
+    private String address;
+
+    @NotNull
+    private Long townId;
+
+    @NotNull
+    private String birth;
+
+    public UserRegisterDTO() {
+    }
+
+    public UserRegisterDTO(String name, String tel, String loginId, String loginPw, String address, Long townId, String birth) {
+        super(name, tel, loginId, loginPw);
+        this.address = address;
+        this.townId = townId;
+        this.birth = birth;
+    }
+}
