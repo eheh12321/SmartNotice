3448a2343d72f83b205fa7d2f4a24143c6e0ec1e
eheh12321 <eheh12321@gmail.com>
2022-11-21 00:36:38 +0900

    refactoring: 마을 관련 RestController 분리 (#10)
    - View 담당과 Rest 담당 깔끔하게 분리

--- src/main/java/sejong/smartnotice/controller/restController/TownApiController.java
+++ src/main/java/sejong/smartnotice/controller/restController/TownApiController.java
@@ -6,9 +6,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import sejong.smartnotice.domain.Town;
+import sejong.smartnotice.domain.member.AdminType;
+import sejong.smartnotice.domain.member.User;
 import sejong.smartnotice.helper.dto.request.TownModifyRequest;
 import sejong.smartnotice.helper.dto.TownRegisterDTO;
+import sejong.smartnotice.helper.dto.request.register.AdminRegisterDTO;
+import sejong.smartnotice.service.AdminService;
 import sejong.smartnotice.service.TownService;
+import sejong.smartnotice.service.UserService;
 
 import javax.validation.constraints.Positive;
 
@@ -20,11 +26,12 @@ import javax.validation.constraints.Positive;
 public class TownApiController {
 
     private final TownService townService;
+    private final UserService userService;
+    private final AdminService adminService;
 
     @Secured({"ROLE_SUPER"}) // 최고 관리자만 접근 가능
     @PostMapping
     public ResponseEntity<String> register(@ModelAttribute("town") TownRegisterDTO registerDTO) {
-        log.info("== 마을 등록 ==");
         if(registerDTO.getRegionCode() == 1L) {
             throw new IllegalArgumentException("마을을 선택해주세요");
         }
@@ -36,7 +43,6 @@ public class TownApiController {
     @PatchMapping("/{townId}")
     public ResponseEntity<String> modify(@PathVariable @Positive Long townId,
             @ModelAttribute("town") TownModifyRequest modifyDTO) {
-        log.info("== 마을 수정 ==");
         modifyDTO.setId(townId);
         townService.modifyTownInfo(modifyDTO);
         return ResponseEntity.ok("수정을 완료했습니다");
@@ -45,7 +51,6 @@ public class TownApiController {
     @Secured({"ROLE_SUPER"})
     @DeleteMapping("/{townId}")
     public ResponseEntity<String> remove(@PathVariable @Positive Long townId) {
-        log.info("== 마을 삭제 ==");
         townService.delete(townId);
         return ResponseEntity.ok("성공적으로 삭제했습니다");
     }
@@ -56,5 +61,36 @@ public class TownApiController {
                                                              @RequestParam Long adminId) {
         townService.setTownRepresentativeAdmin(townId, adminId);
         return ResponseEntity.ok("대표 관리자를 설정했습니다");
+    }
+
+    // 마을 관리자 등록
+    @Secured({"ROLE_SUPER"})
+    @PostMapping("/{id}/admin/new")
+    public ResponseEntity<String> addTownAdmin(@PathVariable @Positive Long id,
+                               @RequestParam(required = false) Long userId,
+                               @RequestParam(required = false) Long adminId) {
+        Town town = townService.findById(id);
+        if (userId != null && adminId == null) {
+            User user = userService.findById(userId); // 주민 정보 조회
+            AdminRegisterDTO registerDTO = new AdminRegisterDTO(user.getName(), user.getTel(),
+                    user.getAccount().getLoginId(), user.getAccount().getLoginPw(), AdminType.ADMIN);
+            Long newAdminId = adminService.register(registerDTO); // 주민 계정 정보로 관리자 계정 생성
+            user.modifyUserIsAdmin(); // 마을 주민이 마을 관리자라는 상태 표시
+            townService.addTownAdmin(adminService.findById(adminId), town); // 관리자와 마을 연결
+        } else if (userId == null && adminId != null) {
+            townService.addTownAdmin(adminService.findById(adminId), town); // 관리자와 마을 연결
+        } else {
+            log.warn("잘못된 요청입니다");
+        }
+        return ResponseEntity.ok("OK");
+    }
+
+    // 마을 관리자 삭제
+    @Secured({"ROLE_SUPER"})
+    @DeleteMapping("/{id}/admin")
+    public ResponseEntity<String> removeTownAdmin(@PathVariable @Positive Long id,
+                                  @RequestParam Long adminId) {
+        townService.removeTownAdmin(adminService.findById(adminId), townService.findById(id));
+        return ResponseEntity.ok("OK");
     }
 }
