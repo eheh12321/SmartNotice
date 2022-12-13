536e7eb46469b0ab6e27e0835fdbe37e1dbcc032
eheh12321 <eheh12321@gmail.com>
2022-12-13 19:41:50 +0900

    feat: 마을 주민 뷰 페이지네이션 적용 (페이지 당 자료, 정렬)
    - Show [] Entries를 이용해 페이지 당 자료 개수 조절 동작 확인
    - 정렬까지 동작 확인
    - 검색 기능 현재 동작 X

--- src/main/java/sejong/smartnotice/controller/restController/UserApiController.java
+++ src/main/java/sejong/smartnotice/controller/restController/UserApiController.java
@@ -4,10 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sejong.smartnotice.helper.dto.response.DataTableResponse;
 import sejong.smartnotice.helper.dto.response.UserResponse;
 import sejong.smartnotice.service.UserService;
@@ -21,18 +20,27 @@ public class UserApiController {
 
     private final UserService userService;
 
+    // 테이블 컬럼 순서 (정렬을 위해 필요)
+    private static final String[] tableColumns = new String[]{"id", "name", "birth", "tel", "address", "townName", "supporterList"};
+
     @GetMapping
     public ResponseEntity<DataTableResponse<?>> getUser(
             Integer draw,
             Integer start,
-            Integer length) {
-        PageRequest pageRequest = PageRequest.of(start, length);
+            Integer length,
+            @RequestParam(name = "order[0][column]") Integer column,
+            @RequestParam(name = "order[0][dir]") String direction) {
+
+        int startPage = (start / length);
+        //startPage에서 length개 가져오겠다
+        // column 기준으로 direction 방향 정렬
+        PageRequest pageRequest = PageRequest.of(startPage, length, Sort.Direction.fromString(direction), tableColumns[column]);
         Page<UserResponse> userPages = userService.findAll(pageRequest);
 
         return ResponseEntity.ok(DataTableResponse.<UserResponse>builder()
                 .draw(draw)
-                .recordsTotal(userPages.getTotalPages())
-                .recordsFiltered(userPages.getTotalPages())
+                .recordsTotal((int)userPages.getTotalElements())
+                .recordsFiltered((int)userPages.getTotalElements())
                 .data(userPages.getContent())
                 .build());
     }
